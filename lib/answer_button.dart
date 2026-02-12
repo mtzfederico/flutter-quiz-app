9457a002a2d2792cef620198102f9c3fe9362591
Federico Martinez <fede@mtzfederico.com>
2026-02-12 12:11:19 -0500

    Made AnswerButton Widget

---
+++ lib/answer_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.answerText,
+    required this.onTap,
+  });
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        backgroundColor: Color.fromARGB(255, 70, 35, 139),
+        foregroundColor: Colors.white,
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -18,14 +19,14 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text("This is the question", style: TextStyle(color: Colors.white, fontSize: 24),),
-            const SizedBox(height: 20),
-            ElevatedButton(onPressed: () {}, child: const Text("Answer1")),
+            const SizedBox(height: 10),
+            AnswerButton(answerText: "answerText", onTap: (){}),
             const SizedBox(height: 5),
-            ElevatedButton(onPressed: () {}, child: const Text("Answer2")),
+            AnswerButton(answerText: "answerText", onTap: (){}),
             const SizedBox(height: 5),
-            ElevatedButton(onPressed: () {}, child: const Text("Answer3")),
+            AnswerButton(answerText: "answerText", onTap: (){}),
             const SizedBox(height: 5),
-            ElevatedButton(onPressed: () {}, child: const Text("Answer4")),
+            AnswerButton(answerText: "answerText", onTap: (){}),
           ],
         ),
       );
