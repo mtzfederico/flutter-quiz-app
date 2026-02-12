8eaddfb7a94200e63b23b9e0efeebfbeee87614f
Federico Martinez <fede@mtzfederico.com>
2026-02-12 11:44:49 -0500

    Created basic question view

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -4,7 +4,7 @@ class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
   @override
-  State<QuestionsScreen> createState(){
+  State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
   }
 }
@@ -12,6 +12,21 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text("Question Screen");
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text("This is the question"),
+          const SizedBox(height: 20),
+          ElevatedButton(onPressed: () {}, child: const Text("Answer1")),
+          const SizedBox(height: 5),
+          ElevatedButton(onPressed: () {}, child: const Text("Answer2")),
+          const SizedBox(height: 5),
+          ElevatedButton(onPressed: () {}, child: const Text("Answer3")),
+          const SizedBox(height: 5),
+          ElevatedButton(onPressed: () {}, child: const Text("Answer4")),
+        ],
+      ),
+    );
   }
 }
