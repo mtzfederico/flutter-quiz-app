a12533f089eaa23b9742d1a255683a8b540b5a5b
Federico Martinez <fede@mtzfederico.com>
2026-02-12 12:19:56 -0500

    Added onTap function

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,20 +14,26 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQustion = questions[0];
+
+    void answerTapped(int index) {
+      print("Answer $index tapped");
+    }
+
     return SizedBox(
       width: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("This is the question", style: TextStyle(color: Colors.white, fontSize: 24),),
+            Text(currentQustion.question, style: TextStyle(color: Colors.white, fontSize: 18),),
             const SizedBox(height: 10),
-            AnswerButton(answerText: "answerText", onTap: (){}),
+            AnswerButton(answerText: currentQustion.answers[0], onTap: (){answerTapped(0);}),
             const SizedBox(height: 5),
-            AnswerButton(answerText: "answerText", onTap: (){}),
+            AnswerButton(answerText: currentQustion.answers[1], onTap: (){answerTapped(1);}),
             const SizedBox(height: 5),
-            AnswerButton(answerText: "answerText", onTap: (){}),
+            AnswerButton(answerText: currentQustion.answers[2], onTap: (){answerTapped(2);}),
             const SizedBox(height: 5),
-            AnswerButton(answerText: "answerText", onTap: (){}),
+            AnswerButton(answerText: currentQustion.answers[3], onTap: (){answerTapped(3);}),
           ],
         ),
       );
